55e35f83ff928559efd05c3519f5109f4ae6ed28
Zheng Zhao <zgbkdlm@gmail.com>
2022-09-20 14:00:29 +0200

    rework some jax codes to numpy and upload latex

--- scripts/regression.py
+++ scripts/regression.py
@@ -7,14 +7,9 @@ Zheng Zhao 2022
 zheng.zhao@it.uu.se
 """
 import math
-import jax
-import jax.numpy as jnp
-import jax.scipy
+import scipy
+import numpy as np
 import matplotlib.pyplot as plt
-from functools import partial
-from jax.config import config
-
-config.update("jax_enable_x64", True)
 
 ell, sigma = 1., 1.
 a, b = 0.2, 1.
@@ -23,41 +18,38 @@ a, b = 0.2, 1.
 def m(t):
     """Mean function.
     """
-    return jnp.sin(2 * t)
+    return np.sin(2 * t)
 
 
-@partial(jax.vmap, in_axes=[0, None])
-@partial(jax.vmap, in_axes=[None, 0])
-def cov_m32(t, s):
+def cov_m32(ts, ss):
     """Matern 3/2 covariance function
     """
-    p = math.sqrt(3) * jnp.abs(t - s) / ell
-    return sigma ** 2 * (1 + p) * jnp.exp(-p)
+    p = math.sqrt(3) * np.abs(ts[:, None] - ss[None, :]) / ell
+    return sigma ** 2 * (1 + p) * np.exp(-p)
 
 
 # Times
 T = 1000
-ts = jnp.linspace(0., 5., T)
+ts = np.linspace(0., 5., T)
 
 # GP mean vector and covariance matrix
 ms = m(ts)
 cov = cov_m32(ts, ts)
 
 # Generate trajectory
-key = jax.random.PRNGKey(999)
-xs = ms + jnp.linalg.cholesky(cov) @ jax.random.normal(key, (T,))
+np.random.seed(666)
+xs = ms + np.linalg.cholesky(cov) @ np.random.randn(T)
 
 # Generate measurements
 xi = 1.
-key, _ = jax.random.split(key)
-ys = a * xs + b + math.sqrt(xi) * jax.random.normal(key, (T,))
+ys = a * xs + b + math.sqrt(xi) * np.random.randn(T)
 
 # Regression, see Equation (12)
-G = a ** 2 * cov + xi * jnp.eye(T)
-chol = jax.scipy.linalg.cho_factor(G)
+G = a ** 2 * cov + xi * np.eye(T)
+chol = scipy.linalg.cho_factor(G)
 
-posterior_mean = ms + a * cov @ jax.scipy.linalg.cho_solve(chol, ys - (a * ms + b))
-posterior_cov = cov - a ** 2 * cov @ jax.scipy.linalg.cho_solve(chol, cov)
+posterior_mean = ms + a * cov @ scipy.linalg.cho_solve(chol, ys - (a * ms + b))
+posterior_cov = cov - a ** 2 * cov @ scipy.linalg.cho_solve(chol, cov)
 
 # Plot
 plt.rcParams.update({
@@ -71,8 +63,8 @@ plt.plot(ts, xs, c='tab:blue', linestyle='--', linewidth=2, label='True $X_{1:T}
 plt.scatter(ts, ys, s=1, c='tab:purple', edgecolors=None, alpha=0.3, label='Measurements $Y_{1:T}$')
 plt.plot(ts, posterior_mean, c='black', linewidth=2, label='Posterior mean')
 plt.fill_between(ts,
-                 posterior_mean - 1.96 * jnp.sqrt(jnp.diag(posterior_cov)),
-                 posterior_mean + 1.96 * jnp.sqrt(jnp.diag(posterior_cov)),
+                 posterior_mean - 1.96 * np.sqrt(np.diag(posterior_cov)),
+                 posterior_mean + 1.96 * np.sqrt(np.diag(posterior_cov)),
                  color='black',
                  edgecolor='none',
                  alpha=0.15,
